c16d02e11ccf2383d1471cc2a81cc089ed2e9cfa
mingpepe <mingpepemath@gmail.com>
2022-07-03 14:07:30 +0800

    Parse line of code and skip comments

--- vm/vm.go
+++ vm/vm.go
@@ -1,7 +1,9 @@
 package vm
 
 import (
+	"bufio"
 	"io"
+	"strings"
 )
 
 type VM struct {
@@ -12,5 +14,26 @@ func New() *VM {
 }
 
 func (vm *VM) Compile(reader io.Reader) ([]byte, error) {
+	scanner := bufio.NewScanner(reader)
+	lines := make([]string, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !skip(line) {
+			lines = append(lines, line)
+		}
+	}
 	return nil, nil
+}
+
+func skip(line string) bool {
+	s := strings.TrimSpace(line)
+	if s == "" {
+		return true
+	}
+
+	if strings.HasPrefix(s, "//") {
+		return true
+	}
+
+	return false
 }
