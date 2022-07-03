d902dec3e0abaea9938018aa6a9569693e2b5d43
mingpepe <mingpepemath@gmail.com>
2022-07-03 10:14:38 +0800

    Add input file for VM

--- executable/vm/main.go
+++ executable/vm/main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/mingpepe/Nand2teris/vm"
 )
 
+func exist(name string) bool {
+	_, err := os.Stat(name)
+	return err == nil
+}
+
 func main() {
+	var filename = flag.String("f", "input.vm", "input filename")
+	flag.Parse()
+
+	if !exist(*filename) {
+		log.Printf("file not found: %s", *filename)
+		return
+	}
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
 	v := vm.New()
-	_, err := v.Compile(nil)
+	asm, err := v.Compile(f)
 	if err != nil {
 		log.Print(err.Error())
 	}
+
+	fmt.Println(asm)
 }
