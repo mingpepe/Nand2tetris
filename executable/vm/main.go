6b1b133fd4ffe14e0f9183b56caa51fa3c8b0770
mingpepe <mingpepemath@gamil.com>
2022-07-04 20:29:31 +0800

    Add argument for directory contains vm files

--- executable/vm/main.go
+++ executable/vm/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/mingpepe/Nand2teris/vm"
@@ -17,45 +18,74 @@ func exist(name string) bool {
 func main() {
 	var filename = flag.String("f", "input.vm", "input filename")
 	var bypass_bootstrap = flag.Bool("bypass", false, "bypass bootstrap code for test")
+	var directory = flag.String("d", "", "directory contains vm files")
 	flag.Parse()
 
-	if !exist(*filename) {
-		log.Printf("file not found: %s", *filename)
-		return
-	}
+	filenames := make([]string, 0)
+	out_filename := ""
+	if *directory == "" {
+		if !exist(*filename) {
+			log.Printf("file not found: %s", *filename)
+			return
+		}
 
-	if !strings.HasSuffix(*filename, ".vm") {
-		log.Printf("input must be a vm file")
-		return
-	}
+		if !strings.HasSuffix(*filename, ".vm") {
+			log.Printf("input must be a vm file")
+			return
+		}
+		filenames = append(filenames, *filename)
 
-	f, err := os.Open(*filename)
-	if err != nil {
-		log.Fatal(err)
+	} else {
+		err := filepath.Walk(*directory, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() {
+				if strings.HasSuffix(path, ".vm") {
+					filenames = append(filenames, path)
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	defer f.Close()
 
 	v := vm.New()
-	boolstrap := ""
+	code := ""
 	if !*bypass_bootstrap {
-		boolstrap = v.BootstrapCode()
-	}
-	asm, err := v.Compile(f)
-	if err != nil {
-		log.Print(err.Error())
+		code += v.BootstrapCode()
 	}
 
-	idx := strings.LastIndex(*filename, ".")
-	out_filename := (*filename)[:idx] + ".asm"
+	for _, _filename := range filenames {
+		f, err := os.Open(_filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		asm, err := v.Compile(f)
+		if err != nil {
+			log.Print(err.Error())
+		}
+		code += asm
+	}
 
+	if *directory == "" {
+		idx := strings.LastIndex(*filename, ".")
+		out_filename = (*filename)[:idx] + ".asm"
+	} else {
+		idx := strings.LastIndex(*directory, "\\")
+		dir_name := (*directory)[idx+1:]
+		out_filename = *directory + "\\" + dir_name + ".asm"
+	}
 	out_f, err := os.Create(out_filename)
 	if err != nil {
 		log.Print(err.Error())
 	}
 	defer out_f.Close()
-	_, err = out_f.WriteString(boolstrap + asm)
+	_, err = out_f.WriteString(code)
 	if err != nil {
 		log.Print(err.Error())
 	}
-	f.Sync()
 }
