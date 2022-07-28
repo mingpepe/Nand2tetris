31e4ca4f5d2eae95b2763f8d96efbcdb2711783c
mingpepe <mingpepemath@gamil.com>
2022-07-28 22:05:40 +0800

    Implement partial SymbolTable with simple data structure

--- analyzer/symbol_table.go
+++ analyzer/symbol_table.go
@@ -1,6 +1,14 @@
 package analyzer
 
 type SymbolTable struct {
+	table          map[string]Variable
+	kindCountTable map[int]int
+}
+
+type Variable struct {
+	varType string
+	kind    int
+	index   int
 }
 
 const (
@@ -14,6 +22,10 @@ const (
 
 func NewSymbolTable() *SymbolTable {
 	s := SymbolTable{}
+	s.kindCountTable[SYMBOL_STATIC] = 0
+	s.kindCountTable[SYMBOL_FIELD] = 0
+	s.kindCountTable[SYMBOL_ARG] = 0
+	s.kindCountTable[SYMBOL_VAR] = 0
 	return &s
 }
 
@@ -22,21 +34,26 @@ func (s *SymbolTable) StartSubroutine() {
 }
 
 func (s *SymbolTable) Define(name, _type string, kind int) {
-
+	var variable Variable
+	variable.varType = _type
+	variable.kind = kind
+	variable.index = s.kindCountTable[kind]
+	s.table[name] = variable
+	s.kindCountTable[kind]++
 }
 
 func (s *SymbolTable) VarCount(kind int) int {
-	return 0
+	return s.kindCountTable[kind]
 }
 
 func (s *SymbolTable) KingOf(name string) int {
-	return SYMBOL_NONE
+	return s.table[name].kind
 }
 
 func (s *SymbolTable) TypeOf(name string) string {
-	return ""
+	return s.table[name].varType
 }
 
 func (s *SymbolTable) IndexOf(name string) int {
-	return 0
+	return s.table[name].index
 }
