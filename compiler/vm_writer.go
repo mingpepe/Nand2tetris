748d728347d2d8c87e0f4ede8a14f0ba718a5bfd
mingpepe <mingpepemath@gamil.com>
2022-08-02 06:35:16 +0800

    Implement VMWriter

--- compiler/vm_writer.go
+++ compiler/vm_writer.go
@@ -1,6 +1,9 @@
 package compiler
 
-import "io"
+import (
+	"io"
+	"strconv"
+)
 
 type VMWriter struct {
 	writer io.Writer
@@ -12,38 +15,49 @@ func NewVMWriter(writer io.Writer) *VMWriter {
 	return &vm
 }
 
-func (v *VMWriter) WritePush(segment, index int) {
-
+func (v *VMWriter) WritePush(segment string, index int) {
+	v.writeVMCode("push", segment, strconv.Itoa(index))
 }
 
-func (v *VMWriter) WritePop(segment, index int) {
-
+func (v *VMWriter) WritePop(segment string, index int) {
+	v.writeVMCode("pop", segment, strconv.Itoa(index))
 }
 
-func (v *VMWriter) WriteArithmetic(cmd int) {
-
+func (v *VMWriter) WriteArithmetic(cmd string) {
+	v.writeVMCode(cmd)
 }
 
 func (v *VMWriter) WriteLabel(lable string) {
-
+	v.writeVMCode("label", lable)
 }
 
 func (v *VMWriter) WriteGoTo(lable string) {
-
+	v.writeVMCode("goto", lable)
 }
 
-func (v *VMWriter) WriteIf(label string) {
-
+func (v *VMWriter) WriteIf(lable string) {
+	v.writeVMCode("if-goto", lable)
 }
 
 func (v *VMWriter) WriteCall(name string, nArgs int) {
-
+	v.writeVMCode("call", name, strconv.Itoa(nArgs))
 }
 
 func (v *VMWriter) WriteFunction(name string, nLocals int) {
-
+	v.writeVMCode("function", name, strconv.Itoa(nLocals))
 }
 
 func (v *VMWriter) WriteReturn() {
+	v.writeVMCode("return")
+}
 
+func (v *VMWriter) writeVMCode(cmd string, args ...string) {
+	if len(args) >= 1 {
+		cmd += " " + args[0]
+	}
+	if len(args) >= 2 {
+		cmd += " " + args[1]
+	}
+	cmd += "\n"
+	v.writer.Write([]byte(cmd))
 }
